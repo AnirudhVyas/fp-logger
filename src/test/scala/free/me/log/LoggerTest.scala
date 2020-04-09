45a8d874a344b53bd387f9c6503ef3406008f854
ricky.nj@gmail.com <ricky.nj@gmail.com>
2020-04-09 16:32:13 -0700

    comments added

--- src/test/scala/free/me/log/LoggerTest.scala
+++ src/test/scala/free/me/log/LoggerTest.scala
@@ -1,15 +1,17 @@
 package free.me.log
 import cats.effect.{IO, Sync}
 object LoggerTest extends App {
-  val ioLogger =
-    Logger.safeLogger[IO](this.getClass.getName)
+  val ioLogger = Logger.safeLogger[IO](this.getClass.getName)
   Sync[IO]
+  // delay some operation -- here for testing it happens to be throw new Exception("yoyo")
     .delay[String](throw new Exception("yoyo"))
-    .attempt
+    .attempt // attempt makes it either.
     .flatMap {
-      case Left(value) =>
-        ioLogger.flatMap(e => e.error(s"failed due to: $value"))
-      case Right(v) => Sync[IO].pure[String](v)
+      case Left(value) => ioLogger.flatMap(e => e.error(s"failed due to: $value"))
+      case Right(v)    =>
+        // if all went well use this.
+        Sync[IO].pure[String](v)
     }
+    // finally end of the world
     .unsafeRunSync()
 }
